c1e0af22c1e96179855af0d17608755bbb645d36
evrwaeqr <evrwaeqr@gmail.com>
2025-09-15 10:30:37 -0300

    refactor: clean default folders

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,14 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/use-color-scheme'
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-}
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name='(protected)' options={{ headerShown: false }} />
-        <Stack.Screen name='sign-in' />
-      </Stack>
+    <Stack>
+      <Stack.Screen name='(protected)' options={{ headerShown: false }} />
+      <Stack.Screen name='sign-in' />
+      <Stack.Screen name='not_found' />
       <StatusBar style='auto' />
-    </ThemeProvider>
+    </Stack>
   )
 }
